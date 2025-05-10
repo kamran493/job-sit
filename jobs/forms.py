34c0b5537998a47da9aa09a923e02e804cd6ddbf
kamran493 <kamalzeraati1335@gmail.com>
2025-05-10 03:31:19 +0330

    اصلاح فرم و نمایش پیام موفقیت

--- jobs/forms.py
+++ jobs/forms.py
@@ -5,7 +5,17 @@ class ApplicationForm(forms.ModelForm):
     class Meta:
         model = JobApplication
         fields = ['name', 'email', 'phone', 'resume', 'cover_letter']
-    
+        widgets = {
+            'cover_letter': forms.Textarea(attrs={'rows': 4}),
+        }
+        labels = {
+            'name': 'نام',
+            'email': 'ایمیل',
+            'phone': 'شماره تماس',
+            'resume': 'رزومه',
+            'cover_letter': 'متن درخواست',
+        }
+
     def __init__(self, *args, **kwargs):
         super(ApplicationForm, self).__init__(*args, **kwargs)
         for field in self.fields.values():
--- jobs/models.py
+++ jobs/models.py
@@ -5,8 +5,9 @@ class JobApplication(models.Model):
     name = models.CharField(max_length=100, verbose_name="نام")
     email = models.EmailField(verbose_name="ایمیل")
     phone = models.CharField(max_length=15, verbose_name="شماره تماس")
-    resume = models.FileField(upload_to='resumes/', verbose_name="رزومه")
-    cover_letter = models.TextField(verbose_name="متن درخواست")
+    resume_file = models.FileField(upload_to='resumes/', verbose_name="فایل رزومه", blank=True, null=True)
+    resume_text = models.TextField(verbose_name="توضیحات رزومه", blank=True)
+    cover_letter = models.TextField(verbose_name="متن درخواست", blank=True)
     submitted_at = models.DateTimeField(default=timezone.now, verbose_name="زمان ارسال")
 
     def __str__(self):
