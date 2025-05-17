6912ad3af7162bae62569285b17b2563e1f80e89
kamran493 <kamalzeraati1335@gmail.com>
2025-05-17 14:45:30 +0330

    اجازه اجرای فایل build.sh داده شد

--- job_sit/build.sh
+++ job_sit/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
+# اجرای دستورات پس از نصب
 
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py collectstatic --noinput
