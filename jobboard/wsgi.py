11e541c59dc198755f9be5451df010d177045868
kamran493 <kamalzeraati1335@gmail.com>
2025-05-19 17:19:43 +0330

    add reset_admin script and update wsgi to run it

--- jobboard/wsgi.py
+++ jobboard/wsgi.py
@@ -14,3 +14,15 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobboard.settings')
 
 application = get_wsgi_application()
+# job_sit/wsgi.py
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'job_sit.settings')
+application = get_wsgi_application()
+
+# --- کد اجرای reset_admin ---
+try:
+    from reset_admin import *
+except Exception as e:
+    print("خطا در تغییر رمز:", e)
