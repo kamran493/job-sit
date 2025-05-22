561730b26b45db547f72c720703c3d1ece09e129
kamran493 <kamalzeraati1335@gmail.com>
2025-05-22 04:11:48 +0330

    reset admin password using script

--- jobboard/wsgi.py
+++ jobboard/wsgi.py
@@ -8,21 +8,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobboard.settings')
 
-application = get_wsgi_application()
-# job_sit/wsgi.py
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'job_sit.settings')
-application = get_wsgi_application()
-
-# --- کد اجرای reset_admin ---
+# --- اجرای اسکریپت ریست رمز عبور فقط یک بار ---
 try:
     from reset_admin import *
 except Exception as e:
-    print("خطا در تغییر رمز:", e)
+    print("خطا در اجرای reset_admin:", e)
+
+application = get_wsgi_application()
+
